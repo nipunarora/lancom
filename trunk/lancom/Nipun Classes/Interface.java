e46a8bfc8713f74cf3a723af35eaee631bdda278
Nipun2512 <Nipun2512@e927147d-814b-0410-9171-874db5f3ba15>
2008-04-24 03:08:41 +0000

    Interface data structure updated

--- trunk/lancom/Nipun Classes/Interface.java
+++ trunk/lancom/Nipun Classes/Interface.java
@@ -7,7 +7,7 @@
  * and open the template in the editor.
  */
 
-package Lancom;
+
 import java.io.*;
 import java.util.*;
 import java.lang.*;
@@ -16,18 +16,25 @@ import java.lang.*;
  *
  * @author Nipun Arora
  */
-public class Interface {
+public class Interface 
+{
     
     String name;
     Ipaddress ipAddress;
     Ipaddress netMask;
     Ipaddress defaultGateway;
+    Vector dns;
     
     
     /** Creates a new instance of Interface */
-    public Interface() 
+    public Interface(String n, String ip, String net, String def, Vector ipaddr) 
     {
-        
+        name = new String(n);
+        ipAddress = new Ipaddress(ip);
+        netMask = new Ipaddress(net);
+        defaultGateway = new Ipaddress(def);
+        dns = new Vector(ipaddr);
     }
+    
     
 }
