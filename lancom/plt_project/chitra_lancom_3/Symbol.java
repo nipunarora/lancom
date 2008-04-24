e9a7b1764bf09aca32b4ceaf20a3bb47995aabeb
sambuddhochakravarty <sambuddhochakravarty@e927147d-814b-0410-9171-874db5f3ba15>
2008-04-24 01:55:14 +0000

    these files werent added previously

---
+++ lancom/plt_project/chitra_lancom_3/Symbol.java
@@ -0,0 +1,69 @@
+public class Symbol {
+    public String name;
+    public String type;
+    Object value;
+    
+    public Symbol(String name) 
+    {
+        this.name = name;
+    }
+
+    public Symbol(String name, String type) 
+    {
+        this.name = name;
+        this.type = type;
+    }
+
+    public Symbol(String name, String type, Object val)
+    {
+        this.name = name;
+        this.type = type;
+        this.value = val;
+    } 
+
+    public void setValue(Object val)
+    {
+        this.value = val;
+    } 
+
+    public String getName()
+    {
+	  return this.name;
+    }
+
+    public String getType()
+    {
+	  return this.type;
+    }
+
+    public Object lookupValue()
+    {
+	  return this.value;
+    }
+
+
+    public String getString()
+    {
+	  String val ;
+	  	if(this.type == "int_type_t")
+		{
+			IntType temp = (IntType)this.value;
+			Integer n = temp.getValue();
+			val = n.toString();
+		}
+
+		if(this.type == "policy_type_t")
+		{
+			Policy temp = (Policy)this.value;
+			val = temp.pol[1];
+			//val = n.toString();
+		}
+
+
+		else
+		{ 
+			val = this.type;
+		}
+		return val;
+    }
+}
