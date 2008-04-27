55ff38062df919d1233be14917c95fb96b2330a2
sambuddhochakravarty <sambuddhochakravarty@e927147d-814b-0410-9171-874db5f3ba15>
2008-04-27 01:35:38 +0000

    latest

---
+++ trunk/lancom/plt_project/chitra_lancom_3/Role.java
@@ -0,0 +1,37 @@
+/*
+ * Role.java
+ *
+ * Created on April 24, 2008, 2:37 AM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+
+import java.util.*;
+import java.io.*;
+
+/**
+ *
+ * @author Nipun Arora
+ */
+public class Role {
+    
+    Vector <Policy> role;
+    /** Creates a new instance of Role */
+    public Role(Vector <Policy> temp) 
+    {
+        role= new Vector<Policy>(temp);
+    }
+    
+    public String getString()
+    {
+        String temp= new String(" ");
+        Iterator <Policy> iter= role.iterator();
+        while(iter.hasNext())
+        {
+          temp= temp + iter.next().getString();  
+        }
+        return (new String(temp));
+    }
+}
