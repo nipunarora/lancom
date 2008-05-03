710f779b698446f9ce5e1c2e5c16855bd3dc197a
sambuddhochakravarty <sambuddhochakravarty@e927147d-814b-0410-9171-874db5f3ba15>
2008-05-03 01:41:30 +0000

    Context based access control

---
+++ trunk/lancom/plt_project/chitra_lancom_3/Context.java
@@ -0,0 +1,37 @@
+/*
+ * Context.java
+ *
+ * Created on April 24, 2008, 2:37 AM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+
+import java.util.*;
+import java.io.*;
+
+/**
+ *
+ * @author Nipun Arora
+ */
+public class Context {
+    
+    Vector <Policy> context;
+    /** Creates a new instance of Context */
+    public Context(Vector <Policy> temp) 
+    {
+        context= new Vector<Policy>(temp);
+    }
+    
+    public String getString()
+    {
+        String temp= new String(" ");
+        Iterator <Policy> iter= context.iterator();
+        while(iter.hasNext())
+        {
+          temp= temp + iter.next().getString();  
+        }
+        return (new String(temp));
+    }
+}
